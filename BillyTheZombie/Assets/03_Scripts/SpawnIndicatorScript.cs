277bb55c74fe8c1667e73c58cea47851101ac448
DESKTOP-B6OVN7J\SamTOUR <samuel.styles93@gmail.com>
2022-03-30 00:47:07 +0200

    EnemyIndicator Correction - 30.03.2022 - 00.47

--- BillyTheZombie/Assets/03_Scripts/SpawnIndicatorScript.cs
+++ BillyTheZombie/Assets/03_Scripts/SpawnIndicatorScript.cs
@@ -13,16 +13,22 @@ public class SpawnIndicatorScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(_enemySpawner.EnemyTracked[0] != null)
+        if(_enemySpawner.EnemyTracked.Count == 0)
         {
-            Vector3 direction = _playerController.transform.position - _enemySpawner.EnemyTracked[0].transform.position;
-            Quaternion rotation = Quaternion.LookRotation(direction, Vector3.forward);
-            rotation.x = 0f;
-            rotation.y = 0f;
-            _arrow.transform.rotation = rotation;
-                 
-    
+            _arrow.enabled = false;
+            return;
         }
-
+        else
+        {
+            if(_enemySpawner.EnemyTracked[0] != null)
+            {
+                _arrow.enabled = true;
+                Vector3 direction = _playerController.transform.position - _enemySpawner.EnemyTracked[0].transform.position;
+                Quaternion rotation = Quaternion.LookRotation(direction, Vector3.forward);
+                rotation.x = 0f;
+                rotation.y = 0f;
+                _arrow.transform.rotation = rotation;
+            }
+        }
     }
 }
