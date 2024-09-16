61be149f6725fe49714a979d2d47bd594677b354
SStyles93 <samuel.styles93@gmail.com>
2024-09-16 12:21:37 +0200

    Revert to commit 863d043

--- BillyTheZombie/Assets/07_ScriptableObjects/0_SOScripts/PlayerSpawnPositionIndexSO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "SpawnIndex", menuName = "ScriptableObject/SpawnIndex", order = 5)]
-public class PlayerSpawnPositionIndexSO : ScriptableObject
-{
-    public int positionIndex = 0;
-}
--- BillyTheZombie/Assets/07_ScriptableObjects/0_SOScripts/UIActionsSO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "ButtonPrompt", menuName = "ScriptableObject/UI/UIActions", order = 4)]
-public class UIActionsSO : ScriptableObject
-{
-    public Sprite[] actionSprites = new Sprite[4];
-}
